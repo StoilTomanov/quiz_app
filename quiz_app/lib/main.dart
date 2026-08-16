963cab2977016d8aca81749e809395fac774ba08
Stoil Tomanov <stoiltomanov@gmail.com>
2026-08-16 16:40:58 +0300

    Quiz app - simple home page

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'home.dart';
+import 'package:quiz_app/quiz_app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 79, 0, 148),
-        body: SafeArea(child: Home()),
-      ),
-    );
-  }
+  runApp(const QuizApp());
 }
---
+++ quiz_app/lib/quiz_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 79, 0, 148),
+        body: SafeArea(child: Home()),
+      ),
+    );
+  }
+}
