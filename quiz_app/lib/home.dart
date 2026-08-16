473eda1dbd924d7bca785bcf640227c73bf57672
Stoil Tomanov <stoiltomanov@gmail.com>
2026-08-16 17:25:31 +0300

    styles

--- quiz_app/lib/home.dart
+++ quiz_app/lib/home.dart
@@ -10,7 +10,11 @@ class Home extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Image.asset('assets/images/quiz-logo.png', height: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            height: 300,
+            color: Color.fromARGB(205, 52, 193, 228),
+          ),
           SizedBox(height: 80),
           Text(
             'Learn Flutter the fun way!',
@@ -21,9 +25,7 @@ class Home extends StatelessWidget {
             onPressed: () {
               print('Quiz is starting...');
             },
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.white
-            ),
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
             icon: Icon(Icons.arrow_right_alt),
             label: Text(
               'Start Quiz',
